471248616c6315766673083a38f972a4c8444107
nololie <lawrence.lefatle@gmail.com>
2023-01-08 14:44:30 +0200

    New look. Showing one section at a time. NO PROPER STYLE

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ function setGreeting() {
     } else if (hour >= 17 || hour < 00) {
         verb = "evening"
     }
-    var greeting = `Good ${verb} I am Lehlohonolo Lefatle, a full-stack web developer. Welcome to my portfolio site.`;
+    var greeting = `<p>Good ${verb} and welcome to my portfolio site. I am Lehlohonolo Lawrence Lefatle, a full-stack web developer.</p>`;
     document.getElementsByClassName("greeting")[0].innerHTML = greeting;
 }
 
-function showForm() {
-    document.getElementsByTagName("form")[0].style.visibility = "visible";
-    document.getElementsByClassName("detailsHeading")[0].style.visibility = "hidden";
+function toggleMenu(e) {
+
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("Overlay").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+
+    document.getElementById("link1").style.display = "inline";
+    document.getElementById("link3").style.display = "inline";
+
+    if (e.id == "link1") {
+        document.getElementById("projects").style.display = "block";
+        document.getElementById("link1").style.display = "none";
+    } else if (e.id == "link3") {
+        document.getElementById("skills").style.display = "block";
+        document.getElementById("link3").style.display = "none";
+    }
+
 }
 
-// How to send form data straight to email?
-
+function showForm() {
+    document.getElementsByTagName("form")[0].style.display = "block";
+    document.getElementsByClassName("detailsHeading")[0].style.display = "none";
+}
